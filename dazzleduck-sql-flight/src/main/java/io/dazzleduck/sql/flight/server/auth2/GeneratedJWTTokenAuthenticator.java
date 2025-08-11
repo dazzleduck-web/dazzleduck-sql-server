17c89c334b73f59f1c6ab684ce593429b356bfd9
PiyushMahore <156774805+PiyushMahore@users.noreply.github.com>
2025-08-11 14:07:09 +0530

    remove unwanted changes

--- dazzleduck-sql-flight/src/main/java/io/dazzleduck/sql/flight/server/auth2/GeneratedJWTTokenAuthenticator.java
+++ dazzleduck-sql-flight/src/main/java/io/dazzleduck/sql/flight/server/auth2/GeneratedJWTTokenAuthenticator.java
@@ -65,7 +65,6 @@ public class GeneratedJWTTokenAuthenticator extends BearerTokenAuthenticator {
         String bearerToken = AuthUtilities.getValueFromAuthHeader(dummyHeaders, Auth2Constants.BEARER_PREFIX);
 
         if (!Strings.isNullOrEmpty(bearerToken)) {
-            Token.token = bearerToken;
             return authResult; // Already has JWT from AuthUtils
         }
 
@@ -80,8 +79,6 @@ public class GeneratedJWTTokenAuthenticator extends BearerTokenAuthenticator {
                 .claim("clusterId", List.of("cc1", "cc2", "cc3")) // this will be generated dynamically
                 .signWith(key)
                 .compact();
-
-        Token.token = jwt;
 
         return new AuthResult() {
             @Override
